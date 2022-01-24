554bb4df1070c35b522d5f1bf682fcce02dbaaaf
dilner1 <dan.milner871@gmail.com>
2022-01-24 16:13:49 +0000

    adding userInformationHTML

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,13 @@
+function userInformationHTML() {
+    return `
+        <h2>${user.name}
+            <span class='small-name>
+                (@<a href='${user.html_url}' target='_blank'></a>
+            </span>
+        </h2>
+    `
+}
+
 function fetchGitHubInformation(event) {
     var username = $('#gh-username').val();
     if(!username) {
@@ -10,5 +20,22 @@ function fetchGitHubInformation(event) {
             <img src='assets/css/loader.gif' alt='loading...'>
         </div>`
     )
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $('#gh-user-data').html(userInformationHTML(userData));
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $('#hg-user-data').html(`<h2>No info found for user ${username}</h2>`)
+            } else {
+                console.log(errorResponse);
+                $('#hg-user-data').html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
+                );
+            }
+        })
 
 }
