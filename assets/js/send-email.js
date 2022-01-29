69485b200903fe6ab8cd0c463ad135de22b7c261
dilner1 <dan.milner871@gmail.com>
2022-01-29 17:34:52 +0000

    adding .then

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,1 +1,24 @@
-console.log('hello')
+        // emailJS API key service_vjp3blq
+        // emailjs.send("service_vjp3blq","template_27068pe");
+
+function sendMail(contactForm) {
+    emailjs.send('gmail', 'rosie', {
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'project_request': contactForm.projectsummary.value,
+    })
+    .then(
+        function(response) {
+            console.log('SUCCESS', response);
+        },
+        function(error) {
+            console,log('FAILD', error);
+        }
+    )
+}
+
+        // emailjs.send("service_vjp3blq", "template_27068pe", {
+        //     from_name: "Mr User Man",
+        //     to_name: "Daniel Milner",
+        //     message: "I want something that does a thing.",
+        // });
